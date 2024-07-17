c28be515b5151192017d4ce3bf466de92fb22f21
yeonkkk <tjddus0302@gmail.com>
2024-07-17 16:18:24 +0900

    vue 기초 - 2. 데이터 바인딩

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,15 +1,35 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h4> {{products[0]}} </h4>
+    <p>{{ price1 }} 만원 </p> <!-- 데이터 바인딩 -->
+  </div>
+  <div>
+    <h4> {{products[1]}} </h4>
+    <p> {{ price2 }} 만원 </p>
+  </div>
+  <div>
+    <h4> {{products[2]}} </h4>
+    <p> {{ price3 }} 만원 </p>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  // 데이터 보관함
+  data(){
+    return {
+      price1 : 60,
+      price2 : 70,
+      price3 : 80,
+      products : ['역삼동원룸', '천호동원룸', '마포구원룸']
+    }
+  },
   components: {
-    HelloWorld
+    
   }
 }
 </script>
