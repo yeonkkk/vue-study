dc6fc20c9cdf0f8a3f83cc6c8d492208834bc2d0
yeonkkk <tjddus0302@gmail.com>
2024-07-29 16:26:31 +0900

    do it vue 입문: 6-4 까지

--- project/vue-todo/src/components/TodoHeader.vue
+++ project/vue-todo/src/components/TodoHeader.vue
@@ -1,11 +1,17 @@
 <template>
-    <div>header</div>
+    <header>
+        <h1>TODO it</h1>
+    </header>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style>
-
+<style scoped>
+ h1 {
+    color: #2F3B52;
+    font-weight: 900; /* 텍스트 굵기 */
+    margin: 2.5rem 0 1.5rem; /* 텍스트 여백 */
+ }
 </style>
--- project/vue-todo/src/components/TodoInput.vue
+++ project/vue-todo/src/components/TodoInput.vue
@@ -1,11 +1,64 @@
 <template>
-    <div>input</div>
+    <!-- vue template 은 단일 루트를 가져야 한다 -->
+    <div class="inputBox shadow">
+        <input type="text" v-model="newTodoItem" placeholder="Type what you have to do" v-on:keyup.enter="addTodo">
+        <span class="addContainer" v-on:click="addTodo">
+            <i class="addBtn fas fa-plus" aria-hidden="true"></i>
+        </span>
+    </div>
 </template>
 
 <script>
-export default {}
+export default {
+    data() {
+        return {
+            newTodoItem: ''
+        }
+    },
+
+    methods: {
+        addTodo() {
+            if (this.newTodoItem !== "") {
+                // 로컬 스토리지에 데이터 추가 (키 : 값)
+                localStorage.setItem(this.newTodoItem, this.newTodoItem);
+                this.clearInput();
+            }
+        },
+
+        clearInput() {
+            this.newTodoItem = '';
+        }
+    }
+}
 </script>
 
-<style>
-
+<style scoped>
+input:focus {
+    /* 인풋박스 선 스타일 지정 */
+    outline: none;
+}
+.inputBox {
+    background: white;
+    height: 50px;
+    line-height: 50px;
+    /* 인풋박스 둥근 테두리 속성 */
+    border-radius: 5px;
+}
+.inputBox input {
+    border-style: none;
+    font-size: 0.9rem;
+}
+.addContainer {
+    /* 추가 버튼 표시 위치 정의 */
+    float: right;
+    background: linear-gradient(to right, #6478FB, #8763FB);
+    display: block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+}
+.addBtn {
+    color: white;
+    /* 추가 아이콘 수직 정렬 정의 */
+    vertical-align: middle;
+}
 </style>
